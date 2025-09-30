5f82122ae53895fc1cc3fdae92b9834cd3ddf66b
Akash Tiwari <akash.tiwari@nucleusteq.com>
2025-09-30 12:56:19 +0530

    initial commit

---
+++ src/main/java/com/example/cb_create_update_product_poc/CbCreateUpdateProductPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.cb_create_update_product_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbCreateUpdateProductPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbCreateUpdateProductPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cb_create_update_product_poc/CbCreateUpdateProductPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cb_create_update_product_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbCreateUpdateProductPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
